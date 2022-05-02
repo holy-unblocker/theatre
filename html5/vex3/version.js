b427d6af39db8232084b5016b8e42933c7994987
David Reed <x@sys32.dev>
2022-05-02 18:21:28 -0400

    vex 3

--- html5/vex3/version.js
+++ html5/vex3/version.js
@@ -1,2 +1,2 @@
-version="16";
-libs=["./cdn.jsdelivr.net/npm/@orange-games/phaser-cachebuster@2.0/build/phaser-cachebuster.min.js","./cdn.jsdelivr.net/npm/@orange-games/phaser-spine@3.0/build/phaser-spine.min.js","./cdn.jsdelivr.net/npm/@orange-games/phaser-super-storage@1.0/build/phaser-super-storage.min.js","./cdn.jsdelivr.net/npm/@orange-games/splash@3.5/build/splash.min.js"];
+version="tc-463";
+libs=["https://cdn.jsdelivr.net/npm/@azerion/phaser-cachebuster@2.0/build/phaser-cachebuster.min.js","https://cdn.jsdelivr.net/npm/@azerion/phaser-spine@3.0/build/phaser-spine.min.js","https://cdn.jsdelivr.net/npm/@azerion/phaser-super-storage@1.0/build/phaser-super-storage.min.js","https://cdn.jsdelivr.net/npm/@azerion/phaser-i18next@1.0/build/phaser-i18next.min.js"];
